61a45401aba6fdf93effcf5ed38e3da00e4058a9
Jamon <jamonvissering@gmail.com>
2016-11-02 10:16:41 +0100

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Navigatie</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>	
+          <a class="navbar-brand" href="#"><img id="logomenu" src="img/wabble_graph.jpg" alt="wabble"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/">Home</a></li>
+            <li><a href="/over-ons">Over ons</a></li>
+			<li><a href="/websites">Websites</a></li>
+            <li><a href="/applicaties">Applicaties</a></li>
+            <li><a href="/offerte">Offerte aanvragen</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->		
+    </nav>
---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+
+<html lang="nl">
+
+
+
+<head>
+
+	<title>Betaalbare websites en applicaties - Wabble</title>
+
+	
+
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=0, minimal-ui" />
+
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/header.css" rel="stylesheet">
+	<link href="css/home.css" rel="stylesheet">
+
+</head>
+
+
+
+<body>
+
+	<?php include_once("inc/header.php") ?>
+
+
+
+	<section id="homeimage">
+
+		<div class="backgroundphoto">
+
+			<div class="backgroundphototext">
+
+				<div id="backgrndtitle">Kwaliteit hoeft niet duur te zijn</div>
+
+				<div id="backgrndsubtitle">Bij ons heb je al een website vanaf €400,-</div>
+
+				<i class="fa fa-angle-double-down arrow" aria-hidden="true"></i>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	
+
+	<section id="websitesapplicaties">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-6">
+
+					<h2 class="sectiontitle">Websites</h2>
+
+					<div class="sectionsubtitle">Responsive, goed vindbaar en doeltreffend.</div>
+
+					<p>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+					</p>
+
+					<a class="button1">Websites</a>
+
+				</div>
+
+				<div class="col-md-6">
+
+					<h2 class="sectiontitle">Applicaties</h2>
+
+					<div class="sectionsubtitle">Uw wens op maat gemaakt voor Android en iOS</div>
+
+					<p>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+					</p>
+
+					<a class="button1">Applicaties</a>
+
+				</div>
+
+			  </div>
+
+		</div>
+
+	</section>
+
+	
+
+	<section id="overwabble">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-12">
+
+					<div class="titletextcentre">
+
+						Zelf je website beheren via opdrachtprompt
+
+					</div>
+
+					<p>
+
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+					</p>
+
+				</div>
+
+			  </div>
+
+		</div>
+
+	</section>
+
+</body>
+
+
+
+<footer>
+
+	
+
+</footer>
+
+
+
+</html>
